a3c1cb69ea7989b6fbc8b7bfe3bcb9a1d31ae770
Felipe Souza <67338813+flpesouza@users.noreply.github.com>
2023-10-03 15:50:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> pilha = new Stack<char>();
+
+            bool isBemFormada = true;
+
+            Console.WriteLine("Digite a sequencia de parenteses e colchetes: ");
+            string sequencia = Console.ReadLine();
+
+            for(int i=0;i< sequencia.Length;i++) {
+                if (sequencia[i] == '[' || sequencia[i] == '(')
+                {
+                    pilha.Push(sequencia[i]);
+
+                }else if (sequencia[i] == ']')
+                {
+                    char desempilha = pilha.Peek();
+                    if(desempilha == '[')
+                    {
+                        pilha.Pop();
+                    }
+                    else
+                    {
+                        isBemFormada = false;
+                    }
+                }else if (sequencia[i] == ')')
+                {
+                    char desempilha = pilha.Peek();
+                    if(desempilha == '(')
+                    {
+                        pilha.Pop();
+                    }
+                    else
+                    {
+                        isBemFormada = false;
+                    }
+                }
+            }
+
+            if (isBemFormada)
+            {
+                Console.WriteLine("A sequencia é bem-formada");
+            }
+            else
+            {
+                Console.WriteLine("A sequencia é malformada");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
